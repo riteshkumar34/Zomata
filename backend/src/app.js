35d3fba379c9ea1dc0091223ba22784054b3c0c7
riteshkumar34 <riteshandshan@gmail.com>
2026-02-13 14:05:07 +0530

    resolved

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,37 +1,21 @@
-const express = require('express');
-const cookieParser = require('cookie-parser');
-const authRoutes = require('./routes/auth.routes');
-const foodRoutes = require('./routes/food.routes');
-const foodPartnerRoutes = require('./routes/food-partner.routes');
-const cors = require('cors');
+const express=require('express');
+const cookieParser=require('cookie-parser')
+const authRoutes=require('./routes/auth.routes')
+const foodRoutes=require('./routes/food.routes')
+const foodPartnerRoutes=require('./routes/food-partner.routes');
+const cors=require('cors');
 
-const app = express();
-
-// ✅ Allowed Origins
-const allowedOrigins = [
-    "http://localhost:5173",
-    "https://zomata-git-main-riteshkumar34s-projects.vercel.app"
-];
-
+const app=express();
 app.use(cors({
-    origin: function (origin, callback) {
-        // allow requests with no origin (like Postman)
-        if (!origin) return callback(null, true);
-
-        if (allowedOrigins.includes(origin)) {
-            callback(null, true);
-        } else {
-            callback(new Error("Not allowed by CORS"));
-        }
-    },
-    credentials: true
+    origin:"http://localhost:5173",
+    credentials:true
 }));
 
 app.use(cookieParser());
 app.use(express.json());
 
-app.use('/api/auth', authRoutes);
-app.use('/api/food', foodRoutes);
-app.use('/api/food-partner', foodPartnerRoutes);
+app.use('/api/auth',authRoutes);
+app.use('/api/food',foodRoutes);
+app.use('/api/food-partner',foodPartnerRoutes);
 
-module.exports = app;
+module.exports=app;
--- frontend/src/routes/AppRoutes.jsx
+++ frontend/src/routes/AppRoutes.jsx
@@ -15,12 +15,12 @@ const AppRoutes = () => {
     return (
         <Router>
             <Routes>
-                <Route path="/register" element={<ChooseRegister />} />
+                <Route path="/" element={<ChooseRegister />} />
                 <Route path="/user/register" element={<UserRegister />} />
                 <Route path="/user/login" element={<UserLogin />} />
                 <Route path="/food-partner/register" element={<FoodPartnerRegister />} />
                 <Route path="/food-partner/login" element={<FoodPartnerLogin />} />
-                <Route path="/" element={<><Home /><BottomNav /></>} />
+                <Route path="/home" element={<><Home /><BottomNav /></>} />
                 <Route path="/saved" element={<><Saved /><BottomNav /></>} />
                 <Route path="/create-food" element={<CreateFood />} />
                 <Route path="/food-partner/:id" element={<Profile />} />
